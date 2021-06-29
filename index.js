1eb78f8f8f6f73ac834f105b9a7319133fcd01c1
Aditi Singh <aditisinghjeechemistry@gmail.com>
2021-06-29 23:59:51 +0530

    successful changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 const app = express();
-app.use(express.static('public');
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
